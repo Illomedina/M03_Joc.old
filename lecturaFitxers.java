267cf4b628db55f4bf0dc9a16ad1cab1e2855f16
Illomedina <josemedina@iesmontsia.org>
2023-05-10 14:32:26 +0200

    inicio

---
+++ lecturaFitxers.java
@@ -0,0 +1,13 @@
+public class lecturaFitxers{
+    
+    public paraulaJoc[] llegirFitxer(String nomFitxer, paraulaJoc[]nombres){
+    //llegim cada linia del fitxer paraula-partida
+        //Usant el metode Split guardem els valors separats per un ";" a l'array.
+        //Usem l'array per crear nous objectes llistaParaules.
+        //Guardar l'objecte a l'array d'objectes paraulaJoc.
+    //Tornem l'array amb tots els objectes
+    return nombres;
+    }
+
+
+}
