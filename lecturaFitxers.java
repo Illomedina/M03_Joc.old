0dbdb87feaad0d7980715dac9b00fc2a2d5284f0
Illomedina <josemedina@iesmontsia.org>
2023-05-16 16:49:20 +0200

    Co-authored-by: EriickGB <EriickGB@users.noreply.github.com> Co-authored-by: kevintroncho <kevintroncho@users.noreply.github.com> Co-authored-by: PereRF <PereRF@users.noreply.github.com>

--- lecturaFitxers.java
+++ lecturaFitxers.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 public class lecturaFitxers{
-    
-    public paraulaJoc[] llegirFitxer(String nomFitxer, paraulaJoc[]nombres){
-    //llegim cada linia del fitxer paraula-partida
-        //Usant el metode Split guardem els valors separats per un ";" a l'array.
-        //Usem l'array per crear nous objectes llistaParaules.
-        //Guardar l'objecte a l'array d'objectes paraulaJoc.
-    //Tornem l'array amb tots els objectes
-    return nombres;
+    public void llegirFitxer(String document){
+        File paraules_joc = new File("/home/super/Escritorio/historic/paraules-partides.txt");
+        //Mentre hague una linea
+        String linea;
+        try (Scanner sc = new Scanner(paraules_joc, StandardCharsets.UTF_8))
+        {  
+            while(sc.hasNextLine()){
+                linea = sc.nextLine();
+                System.out.println(linea);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+         
+  
+   
     }
-
 
 }
